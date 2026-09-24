452fc441072c30a785c546d91e4a76821afac6f5
sergio522 <sergioandresloaiza71@gmail.com>
2026-09-24 04:02:48 +0000

    cambio 2

--- parcial1-voltacali/src/main/java/co/edu/usc/voltacali/CargadorVE.java
+++ parcial1-voltacali/src/main/java/co/edu/usc/voltacali/CargadorVE.java
@@ -11,10 +11,15 @@ public class CargadorVE {
      * Enum para los tipos de conector.
      */
     public enum TipoConector {
+        /** Conector Tipo 1. */
         TIPO_1,
+        /** Conector Tipo 2. */
         TIPO_2,
+        /** Conector CCS2. */
         CCS2,
+        /** Conector CHADEMO. */
         CHADEMO,
+        /** Conector GBT. */
         GBT
     }
 
@@ -22,11 +27,17 @@ public class CargadorVE {
      * Enum para los tipos de cargador.
      */
     public enum TipoCargador {
+        /** Cargador de pared. */
         MURAL,
+        /** Cargador pedestal. */
         PEDESTAL,
+        /** Cargador rapido DC. */
         RAPIDO_DC,
+        /** Cargador ultrarrapido. */
         ULTRARRAPIDO,
+        /** Cargador portatil. */
         PORTATIL,
+        /** Cargador bidireccional V2G. */
         BIDIRECCIONAL_V2G
     }
 
@@ -34,43 +45,72 @@ public class CargadorVE {
      * Enum para las ubicaciones.
      */
     public enum Ubicacion {
+        /** Ubicacion centro comercial. */
         CENTRO_COMERCIAL,
+        /** Ubicacion universidad. */
         UNIVERSIDAD,
+        /** Ubicacion estacion de servicio. */
         ESTACION_SERVICIO,
+        /** Ubicacion parqueadero publico. */
         PARQUEADERO_PUBLICO,
+        /** Ubicacion residencial. */
         RESIDENCIAL,
+        /** Ubicacion hotel. */
         HOTEL,
+        /** Ubicacion terminal. */
         TERMINAL,
+        /** Ubicacion flota corporativa. */
         FLOTA_CORPORATIVA
     }
 
+    /** Total de cargadores creados. */
     public static int totalCargadores = 0;
+    /** Contador global de registros de sesion. */
     public static int contadorRegistros = 0;
+    /** Limite de potencia de la red en kW. */
     public static final double LIMITE_RED = 50.0;
+    /** Incremento por defecto en kW. */
     public static final double INCREMENTO_DEFECTO = 5.0;
 
+    /** Marca del equipo. */
     private String fabricante;
+    /** Año de instalacion. */
     private int anioInstalacion;
+    /** Voltaje nominal de operacion. */
     private int voltajeNominal;
+    /** Conector principal. */
     private TipoConector tipoConector;
+    /** Categoria del equipo. */
     private TipoCargador tipoCargador;
+    /** Conectores disponibles. */
     private int numeroConectores;
+    /** Puestos de parqueo. */
     private int puestosParqueo;
+    /** Potencia maxima del equipo. */
     private double potenciaMaxima;
+    /** Ubicacion del cargador. */
     private Ubicacion ubicacion;
+    /** Potencia actual entregada. */
     private double potenciaActual;
 
+    /** Bitacora de sesiones del cargador. */
     private Vector<RegistroSesion> bitacora;
 
     /**
      * Clase interna no estatica para registrar las sesiones de carga.
      */
     public class RegistroSesion {
+        /** Identificador del registro. */
         private int idRegistro;
+        /** Marca del fabricante capturada. */
         private String fabricante;
+        /** Año de instalacion capturado. */
         private int anioInstalacion;
+        /** Potencia actual capturada. */
         private double potenciaActual;
+        /** Descripcion del evento. */
         private String evento;
+        /** Estado del evento (valido o no). */
         private boolean valido;
 
         /**
@@ -92,17 +132,27 @@ public class CargadorVE {
         /**
          * Describe el registro.
          *
-         * @return Descripcion formateada.
+         * @return Descripcion formateada del registro.
          */
         public String describir() {
             return String.format("#%d | %s | Año: %d | Pot: %.2f kW | Evento: %s | Valido: %b",
                     idRegistro, fabricante, anioInstalacion, potenciaActual, evento, valido);
         }
 
+        /**
+         * Obtiene si el registro es valido.
+         *
+         * @return true si es valido, false en caso contrario.
+         */
         public boolean isValido() {
             return valido;
         }
 
+        /**
+         * Obtiene la potencia actual registrada.
+         *
+         * @return Potencia actual en kW.
+         */
         public double getPotenciaActual() {
             return potenciaActual;
         }
@@ -124,78 +174,173 @@ public class CargadorVE {
         totalCargadores++;
     }
 
+    /**
+     * Obtiene el fabricante.
+     *
+     * @return Fabricante del equipo.
+     */
     public String getFabricante() {
         return fabricante;
     }
 
+    /**
+     * Establece el fabricante.
+     *
+     * @param fabricante Marca a asignar.
+     */
     public void setFabricante(String fabricante) {
         this.fabricante = fabricante;
     }
 
+    /**
+     * Obtiene el año de instalacion.
+     *
+     * @return Año de instalacion.
+     */
     public int getAnioInstalacion() {
         return anioInstalacion;
     }
 
+    /**
+     * Establece el año de instalacion.
+     *
+     * @param anioInstalacion Año a asignar.
+     */
     public void setAnioInstalacion(int anioInstalacion) {
         this.anioInstalacion = anioInstalacion;
     }
 
+    /**
+     * Obtiene el voltaje nominal.
+     *
+     * @return Voltaje nominal.
+     */
     public int getVoltajeNominal() {
         return voltajeNominal;
     }
 
+    /**
+     * Establece el voltaje nominal.
+     *
+     * @param voltajeNominal Voltaje a asignar.
+     */
     public void setVoltajeNominal(int voltajeNominal) {
         this.voltajeNominal = voltajeNominal;
     }
 
+    /**
+     * Obtiene el tipo de conector.
+     *
+     * @return TipoConector actual.
+     */
     public TipoConector getTipoConector() {
         return tipoConector;
     }
 
+    /**
+     * Establece el tipo de conector.
+     *
+     * @param tipoConector Conector a asignar.
+     */
     public void setTipoConector(TipoConector tipoConector) {
         this.tipoConector = tipoConector;
     }
 
+    /**
+     * Obtiene el tipo de cargador.
+     *
+     * @return TipoCargador actual.
+     */
     public TipoCargador getTipoCargador() {
         return tipoCargador;
     }
 
+    /**
+     * Establece el tipo de cargador.
+     *
+     * @param tipoCargador Tipo de cargador a asignar.
+     */
     public void setTipoCargador(TipoCargador tipoCargador) {
         this.tipoCargador = tipoCargador;
     }
 
+    /**
+     * Obtiene el numero de conectores.
+     *
+     * @return Cantidad de conectores.
+     */
     public int getNumeroConectores() {
         return numeroConectores;
     }
 
+    /**
+     * Establece el numero de conectores.
+     *
+     * @param numeroConectores Cantidad de conectores a asignar.
+     */
     public void setNumeroConectores(int numeroConectores) {
         this.numeroConectores = numeroConectores;
     }
 
+    /**
+     * Obtiene los puestos de parqueo.
+     *
+     * @return Cantidad de puestos.
+     */
     public int getPuestosParqueo() {
         return puestosParqueo;
     }
 
+    /**
+     * Establece los puestos de parqueo.
+     *
+     * @param puestosParqueo Puestos a asignar.
+     */
     public void setPuestosParqueo(int puestosParqueo) {
         this.puestosParqueo = puestosParqueo;
     }
 
+    /**
+     * Obtiene la potencia maxima.
+     *
+     * @return Potencia maxima en kW.
+     */
     public double getPotenciaMaxima() {
         return potenciaMaxima;
     }
 
+    /**
+     * Establece la potencia maxima.
+     *
+     * @param potenciaMaxima Potencia maxima a asignar.
+     */
     public void setPotenciaMaxima(double potenciaMaxima) {
         this.potenciaMaxima = potenciaMaxima;
     }
 
+    /**
+     * Obtiene la ubicacion.
+     *
+     * @return Ubicacion actual.
+     */
     public Ubicacion getUbicacion() {
         return ubicacion;
     }
 
+    /**
+     * Establece la ubicacion.
+     *
+     * @param ubicacion Ubicacion a asignar.
+     */
     public void setUbicacion(Ubicacion ubicacion) {
         this.ubicacion = ubicacion;
     }
 
+    /**
+     * Obtiene la potencia actual.
+     *
+     * @return Potencia actual en kW.
+     */
     public double getPotenciaActual() {
         return potenciaActual;
     }
@@ -215,6 +360,11 @@ public class CargadorVE {
         }
     }
 
+    /**
+     * Obtiene la bitacora de sesiones.
+     *
+     * @return Vector con los registros.
+     */
     public Vector<RegistroSesion> getBitacora() {
         return bitacora;
     }
